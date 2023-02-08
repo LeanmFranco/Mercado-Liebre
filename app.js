4787f21d33c84678db0cbb4ac522091d8715beaa
LeanmFranco <leanmfranco@gmail.com>
2023-02-07 22:37:33 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const path = require('path'); //path es nativo de NODE por lo que ni hay que instalar.
+
+const publicPath = path.join(__dirname, 'public'); //carpeta public estatica o absoluta
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html')); //direccion absoluta
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`server started on http://localhost:${port}`);
+});
+ 
